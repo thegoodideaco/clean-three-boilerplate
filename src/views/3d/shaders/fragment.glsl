e8efdae98326127a80f139517a0490e8a3ac52ab
Jonathan Snyder <jon.jsstudios@gmail.com>
2023-12-30 23:26:42 -0500

    Add 4D noise and thresholding to fragment shader

--- src/views/3d/shaders/fragment.glsl
+++ src/views/3d/shaders/fragment.glsl
@@ -1,4 +1,5 @@
 #pragma glslify: cnoise = require('glsl-noise/classic/3d')
+#pragma glslify: v4noise = require('glsl-noise/classic/4d')
 
 precision mediump float;
 varying vec3 vPos;
@@ -6,12 +7,20 @@ varying vec2 vUv;
 
 uniform float uTime;
 uniform float uScale;
+uniform float uThreshold;
+uniform float uRealTime;
 
 void main() {
     float n = cnoise(vec3(vUv.xy, 1.) * (uTime * uScale));
-    vec3 p = normalize(vPos);
+    float n2 = v4noise(vec4(vec3(vUv.xy, 1.) * (mod(uTime * uScale, 1.)), uRealTime * uScale));
+    vec3 p = (vPos * uScale);
+    p.y += uRealTime * uTime * uScale;
+    n = cnoise(p);
 
-    float alpha = smoothstep(.1, .7, 1. - pow(abs(p.xy * -.5).y, 1.));
+    float alpha = smoothstep(uThreshold, n, 1. - n);
 
-    gl_FragColor = vec4((vUv.xy) * .5, n, alpha);
+    if(alpha == 0.)
+        discard;
+
+    gl_FragColor = vec4((vUv.xy) * .5, dot(p.x, p.y), alpha);
 }
